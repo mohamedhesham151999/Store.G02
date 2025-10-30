6596c361f9763bab3273250f4688720085594684
ASD <ASD@MANI>
2025-10-30 21:03:32 +0200

    Buggy Controller Error Types

---
+++ Infrastructure/Store.G02.Presentation/BuggyController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Presentation
+{
+    public class BuggyController : ControllerBase
+    {
+        [HttpGet("notfound")]
+        public IActionResult GetNotFound() 
+        {
+            return NotFound();
+        }
+
+        [HttpGet("servererror")]
+        public IActionResult GetServerError() 
+        {
+            throw new Exception();
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public IActionResult GetBadRequest()
+        {
+            return BadRequest();
+        }
+
+        [HttpGet("unauthorize")]
+        public IActionResult GetAnAuthorized() 
+        {
+            return Unauthorized();
+        }
+    }
+}
