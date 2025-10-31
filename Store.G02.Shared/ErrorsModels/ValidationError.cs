f3c2ea6289041cdee2f9318101e9c5ba1741f9a1
ASD <ASD@MANI>
2025-10-31 17:24:59 +0200

    Handling Validation Error

---
+++ Store.G02.Shared/ErrorsModels/ValidationError.cs
@@ -0,0 +1,8 @@
+﻿namespace Store.G02.Shared.ErrorsModels
+{
+    public class ValidationError
+    {
+        public string Field { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+    }
+}
