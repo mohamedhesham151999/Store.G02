b0ad820b5274829ca7e0e52c3e6c1e84da7ae5d4
ASD <ASD@MANI>
2025-10-31 20:13:45 +0200

    Refactoring Main Function and Middleware

---
+++ Infrastructure/Store.G02.Persistence/InfraStructureServiceRegistration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Store.G02.Domain.Contracts;
+using Store.G02.Persistence.Data.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Persistence
+{
+    public static class InfraStructureServiceRegistration
+    {
+        public static IServiceCollection AddInfraStructureService(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddScoped<IDbInitializer, DbInitializer>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
---
+++ Store.G02.Web/Extension/Extension.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.G02.Domain.Contracts;
+using Store.G02.Persistence;
+using Store.G02.Services;
+using Store.G02.Services.Abstractions;
+using Store.G02.Services.Mapping;
+using Store.G02.Shared.ErrorsModels;
+using Store.G02.Web.Middleware;
+using System.Threading.Tasks;
+
+namespace Store.G02.Web.Extension
+{
+    public static class Extension
+    {
+        public static IServiceCollection RegisterAllServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddBuiltInService();
+            services.AddSwaggerInService();
+            services.ConfigureServices();
+            
+
+
+            services.AddInfraStructureService(configuration);
+            services.AddApplicationServices(configuration);
+            
+            return services;
+        }
+
+        private static IServiceCollection AddBuiltInService(this IServiceCollection services)
+        {
+            services.AddControllers();
+
+            return services;
+
+        }
+        private static IServiceCollection AddSwaggerInService(this IServiceCollection services)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            return services;
+        }
+        private static IServiceCollection ConfigureServices(this IServiceCollection services)
+        {
+            services.Configure<ApiBehaviorOptions>(config =>
+            {
+                config.InvalidModelStateResponseFactory = (actioncontext) =>
+                {
+                    var errors = actioncontext.ModelState.Where(m => m.Value.Errors.Any())
+                                   .Select(m => new ValidationError()
+                                   {
+                                       Field = m.Key,
+                                       Errors = m.Value.Errors.Select(errors => errors.ErrorMessage)
+                                   });
+
+                    var response = new ValidationErrorResponse()
+                    {
+                        Errors = errors
+                    };
+
+                    return new BadRequestObjectResult(response);
+                };
+            });
+
+            return services;
+        }
+        public static async Task<WebApplication> ConfigureMiddelwares(this WebApplication app)
+        {
+            await app.InitializeDatabaseAsync();
+
+            app.UseGlobalErrorHandling();
+            
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseStaticFiles();  
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            return app;
+        }
+        private static async Task<WebApplication> InitializeDatabaseAsync(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+            await dbInitializer.InitializerAsync();
+            return app;
+        }
+        
+        private static  WebApplication UseGlobalErrorHandling(this WebApplication app)
+        {
+            app.UseMiddleware<GlobalErrorHandlingMiddleware>();
+
+            return app;
+        }
+
+
+    }
+}
