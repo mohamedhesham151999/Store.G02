6124c5bd0251d7f8f249eec8dc6e8925e5413668
ASD <ASD@MANI>
2025-11-03 16:21:05 +0200

    Handled Cash Repository

---
+++ Core/Store.G02.Domain/Contracts/ICasheRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Domain.Contracts
+{
+    public interface ICasheRepository
+    {
+        Task<string?> GetAsync(string key);
+        Task SetAsync(string key, string value, TimeSpan duration);
+    }
+}
---
+++ Infrastructure/Store.G02.Persistence/Repositories/CashRepsitory.cs
@@ -0,0 +1,27 @@
+﻿using StackExchange.Redis;
+using Store.G02.Domain.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Store.G02.Persistence.Repositories
+{
+    public class CashRepsitory(IConnectionMultiplexer connection) : ICasheRepository
+    {
+
+        private readonly IDatabase _database = connection.GetDatabase();
+        public async Task<string?> GetAsync(string key)
+        {
+            var redisValue = await _database.StringGetAsync(key);
+            return redisValue;
+        }
+
+        public async Task SetAsync(string key, string value, TimeSpan duration)
+        {
+           await _database.StringSetAsync(key, JsonSerializer.Serialize(value) ,duration);
+        }
+    }
+}
