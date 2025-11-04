42b8e63d9f323de9c1495cfea661996aa7691d65
ASD <ASD@MANI>
2025-11-05 00:46:28 +0200

    Preparing DataBase and Classes For Auth Service and Auth Controller

---
+++ Infrastructure/Store.G02.Persistence/Identity/Contexts/IdentityStoreDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Store.G02.Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Persistence.Identity.Contexts
+{
+    public class IdentityStoreDbContext(DbContextOptions<IdentityStoreDbContext> options ) : IdentityDbContext<AppUser>(options)
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+            builder.Entity<AppUser>().ToTable("Users");
+            builder.Entity<IdentityRole>().ToTable("Roles");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRole");
+            builder.Entity<Address>().ToTable("Addresses");
+
+            builder.Ignore<IdentityUserClaim<string>>();
+            builder.Ignore<IdentityUserLogin<string>>();
+            builder.Ignore<IdentityUserToken<string>>();
+            builder.Ignore<IdentityRoleClaim<string>>();
+
+        }
+
+
+    }
+}
