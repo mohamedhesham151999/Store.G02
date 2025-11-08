ccc0d297374c1bb2817e7fb1e68bbb6c24d531e1
ASD <ASD@MANI>
2025-11-08 05:34:23 +0200

    Security Module - Auth Service[Login-Register]

---
+++ Core/Store.G02.Services.Abstractions/Auth/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using Store.G02.Shared.Dtos.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Services.Abstractions.Auth
+{
+    public interface IAuthService
+    {
+        Task<UserResponse?> LoginAsync(LoginRequest request);
+        Task<UserResponse?> RegisterAsync(RegisterRequest request);
+    }
+}
--- Core/Store.G02.Services/ServiceManager.cs
+++ Core/Store.G02.Services/ServiceManager.cs
@@ -1,9 +1,13 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Store.G02.Domain.Contracts;
+using Store.G02.Domain.Entities.Identity;
 using Store.G02.Services.Abstractions;
+using Store.G02.Services.Abstractions.Auth;
 using Store.G02.Services.Abstractions.Baskets;
 using Store.G02.Services.Abstractions.Cashe;
 using Store.G02.Services.Abstractions.Products;
+using Store.G02.Services.Auth;
 using Store.G02.Services.Baskets;
 using Store.G02.Services.Cashe;
 using Store.G02.Services.Products;
@@ -11,12 +15,14 @@ using Store.G02.Services.Products;
 
 namespace Store.G02.Services
 {
-    public class ServiceManager(IUnitOfWork _unitOfWork, IMapper _mapper, IBasketRepository _basketRepository , ICasheRepository _cashRepository) : IServiceManager
+    public class ServiceManager(IUnitOfWork _unitOfWork, IMapper _mapper, IBasketRepository _basketRepository , ICasheRepository _cashRepository , UserManager<AppUser> _userManager) : IServiceManager
     {
         public IProductService ProductService { get; } = new ProductService(_unitOfWork, _mapper);
 
         public IBasketService BasketService { get; } = new BasketService(_basketRepository, _mapper);
 
         public ICasheService CasheService { get; } = new CasheService(_cashRepository);
+
+        public IAuthService AuthService { get; } = new AuthService(_userManager);
     }
 }
