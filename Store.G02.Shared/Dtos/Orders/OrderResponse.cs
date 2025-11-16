526d303e85e58ca243c2d23e357c91dbafbc23bf
ASD <ASD@MANI>
2025-11-16 16:06:23 +0200

    IOrderService

---
+++ Store.G02.Shared/Dtos/Orders/OrderResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G02.Shared.Dtos.Orders
+{
+    public class OrderResponse
+    {
+        public Guid Id { get; set; }
+        public string UserEmail { get; set; }
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+        public OrderAddressDto ShippingAddress { get; set; }
+        public string DeliveryMethod  { get; set; }
+        public ICollection<OrderItemDto> Item { get; set; }
+        public decimal SubTotal { get; set; } // price * quantity
+        public decimal Total { get; set; }  
+    }
+}
