5660324b31a33f5d0da95b4e30045cd8131d79f6
Mohamed_Hesham <Mohamed_Hesham@MANIRYZEN>
2025-11-28 11:46:35 +0200

    Add Webhook End Point

--- Infrastructure/Store.G02.Presentation/PaymentsController.cs
+++ Infrastructure/Store.G02.Presentation/PaymentsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Store.G02.Services.Abstractions;
 using Store.G02.Services.Abstractions.Payments;
+using Stripe;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,8 +24,36 @@ namespace Store.G02.Presentation
         }
 
 
+        // stripe listen --forward-to https://localhost:7048/api/payments/webhook
+        [Route("webhook")]
+        [HttpPost]
+        public async Task<IActionResult> Index()
+        {
+            var json = await new StreamReader(HttpContext.Request.Body).ReadToEndAsync();
+            const string endpointSecret = "whsec_2a7db2ec748322243a03138fe171f41c536865777b7f042147dd0aa29258634f";
+            
+                var stripeEvent = EventUtility.ParseEvent(json);
+                var signatureHeader = Request.Headers["Stripe-Signature"];
 
+                stripeEvent = EventUtility.ConstructEvent(json,
+                        signatureHeader, endpointSecret);
 
+                // If on SDK version < 46, use class Events instead of EventTypes
+                if (stripeEvent.Type == EventTypes.PaymentIntentSucceeded)
+                {
+                   // update Order Status to Success
+                }
+                else if (stripeEvent.Type == EventTypes.PaymentIntentPaymentFailed)
+                {
+                // update Order Status to Failed
+            }
+            else
+                {
+                    Console.WriteLine("Unhandled event type: {0}", stripeEvent.Type);
+                }
+                return Ok();
+           
+        }
 
 
     }
